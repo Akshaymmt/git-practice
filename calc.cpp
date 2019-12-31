1617a71e48e4cc9f359f92cc3d03864d196a31aa
Pankaj Gaikar <pankaj.gaikar@mediamagictechnologies.com>
2019-12-31 14:21:34 +0530

    Added addition,substraction,multiplication to merge branch

--- calc.cpp
+++ calc.cpp
@@ -5,8 +5,18 @@ int add(int a,int b)
 {
     return a+b;
 }
-
+int sub( int a,int b )
+{
+    return a-b;
+}
+int mul( int a,int b )
+{
+    return a*b;
+}
 int main()
 {
-    add(1,2);
+    int a=10,b=10;
+    cout << "\n addition -"<<add( a,b );
+    cout << "\n substraction -" << sub( a,b );
+    cout << " \n multiplication - "<< mul( a,b );
 }
