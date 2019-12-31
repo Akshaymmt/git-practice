e786bbf498b959db1e1347023fa87934864bf815
Pankaj Gaikar <pankaj.gaikar@mediamagictechnologies.com>
2019-12-31 14:27:17 +0530

    added division function

--- calc.cpp
+++ calc.cpp
@@ -5,6 +5,9 @@ int add(int c,int b)
 {
     //edited by merge for conflicts practice in rebase
     return c+b;
+
+    //edited to rebase conflict by rebase branch
+
 }
 int sub( int a,int b )
 {
@@ -14,10 +17,15 @@ int mul( int a,int b )
 {
     return a*b;
 }
+int div( int a,int b )
+{
+    return a/b;
+}
 int main()
 {
     int a=10,b=10;
     cout << "\n addition -"<<add( a,b );
     cout << "\n substraction -" << sub( a,b );
     cout << " \n multiplication - "<< mul( a,b );
+    cout << " \n division -" << div( a,b ); 
 }
